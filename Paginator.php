4573e5c2a400b4d2078fd0211934de93a4d2c21d
hlf <felon527@qq.com>
2015-07-23 19:46:16 +0800

    Update Paginator.php

--- Paginator.php
+++ Paginator.php
@@ -2,6 +2,12 @@
 
 /**
  * 分页器
+ * 需要添加的功能：
+ * 1.获取总记录数的属性；
+ * 2.获取当前网页的总记录数的方法；
+ * 3.添加一个GoPage方法：可以在输入框输入一个页码，然后跳转到输入的页码的页面；
+ * 4.添加一个下拉框，列出所有的页面，用于从下拉框选择一个页面；
+ * 5.将首页，上一页，下一页，末页的文字进行参数化：用变量替换。
  */
 class Paginator {
 
@@ -25,8 +31,49 @@ class Paginator {
     private $pagesInRange;
     // 请求的uri
     private $uri = null;
+    //
+    //当前页上第一条记录是整个记录集的第几条
+    private $firstItemNumber;
+    //第一个显示的页码
+    private $firstPageInRange;
+    //本页上的记录有几条
+    private $currentItemCount;
+    //当前页上最后一条记录是整个记录集种的第几条
+    private $lastItemNumber;
+    //最后一个显示的页码
+    private $lastPageInRange;
 
-    public function __construct($uri, $totalItemCount, $pageSize = 10, $current = 1, $pageRange = 5) {
+    //获取页码数组
+    public function getPagesInRange() {
+        return $this->pagesInRange;
+    }
+
+    public function getFirstItemNumber() {
+        if (count($this->pagesInRange) > 0) {
+            return $this->pagesInRange[0];
+        } else {
+            return null;
+        }
+    }
+
+    public function getLastItemNumber() {
+        $rangeCount = count($this->pagesInRange);
+        if ($rangeCount > 0) {
+            return $this->pagesInRange[$rangeCount];
+        } else {
+            return null;
+        }
+    }
+
+    /**
+     * 
+     * @param type $uri：页码上的请求地址
+     * @param type $totalItemCount：总记录数
+     * @param type $current：当前页码
+     * @param type $pageSize：分页大小
+     * @param type $pageRange：要显示的页码范围，默认显示5个分页。
+     */
+    public function __construct($uri, $totalItemCount, $current = 1, $pageSize = 10, $pageRange = 5) {
         $this->uri = $uri;
         $this->totalItemCount = $totalItemCount; // 总记录数
         $this->pageSize = $pageSize; // 每页显示多少条记录
@@ -57,7 +104,7 @@ class Paginator {
             $this->pagesInRange = range(1, $this->pageCount);
         } else {
             $half = ceil($pageRange / 2);
-            if ($this->current - $half <= 0) {
+            if ($this->current < $half) {
                 /*
                   for ($i = 1; $i <= $pageRange; $i ++) {
                   array_push($this->pagesInRange, $i);
@@ -86,6 +133,9 @@ class Paginator {
         }
     }
 
+    /**
+     * 生成页码
+     */
     public function createPageLinks() {
         $html = "<div>";
         if ($this->first < $this->current) {
@@ -110,6 +160,9 @@ class Paginator {
         echo $html;
     }
 
+    /**
+     * 生成bootstrap风格页码
+     */
     public function createBootstrapPageLinks() {
         $html = "<div class='pagination pagination-small'><ul>";
         if ($this->first < $this->current) {
